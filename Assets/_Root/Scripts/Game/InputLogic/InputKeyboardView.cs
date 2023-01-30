0b32cae75ce2569bf8725a44a8c62a890cfa55ee
alexandr.kulikov <a.kulikov@tascredit.kz>
2023-01-30 18:15:00 +0600

    Lesson1
    Lesson1

---
+++ Assets/_Root/Scripts/Game/InputLogic/InputKeyboardView.cs
@@ -0,0 +1,33 @@
+using JoostenProductions;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace Game.InputLogic
+{
+    internal class InputKeyboardView : BaseInputView
+    {
+        [SerializeField] private float _inputMultiplier = 10;
+        private const string _horizontal = "Horizontal";
+
+
+        private void Start() =>
+            UpdateManager.SubscribeToUpdate(Move);
+
+        private void OnDestroy() =>
+            UpdateManager.UnsubscribeFromUpdate(Move);
+
+        private void Move()
+        {
+            float axisOffset = Input.GetAxis(_horizontal);
+            float moveValue = _inputMultiplier * Time.deltaTime * axisOffset;
+
+            float abs = Mathf.Abs(moveValue);
+            float sign = Mathf.Sign(moveValue);
+
+            if (sign > 0)
+                OnRightMove(abs);
+            else if (sign < 0)
+                OnLeftMove(abs);
+        }
+    }
+}
---
+++ Assets/_Root/Scripts/Ui/SettingsMenuView.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Ui
+{
+    public class SettingsMenuView : MonoBehaviour
+    {
+        [SerializeField] private Button _buttonBack;
+
+
+        public void Init(UnityAction backToMain)
+        {
+            _buttonBack.onClick.AddListener(backToMain);
+        }
+
+        public void OnDestroy()
+        {
+            _buttonBack.onClick.RemoveAllListeners();
+        }
+    }
+}
