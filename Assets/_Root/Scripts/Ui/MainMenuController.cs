2d9f231f6d98c6bfc00584a902d308f281eaa8c8
alexandr.kulikov <a.kulikov@tascredit.kz>
2023-02-02 22:51:20 +0600

    Lesson2
    Lesson2

--- Assets/_Root/Scripts/Ui/MainMenuController.cs
+++ Assets/_Root/Scripts/Ui/MainMenuController.cs
@@ -2,6 +2,8 @@ using Profile;
 using Tool;
 using UnityEngine;
 using Object = UnityEngine.Object;
+using Services.Ads.UnityAds;
+using Services.IAP;
 
 namespace Ui
 {
@@ -11,12 +13,17 @@ namespace Ui
         private readonly ProfilePlayer _profilePlayer;
         private readonly MainMenuView _view;
 
+        private UnityAdsService _adsService;
+        private IAPService _iapService;
 
-        public MainMenuController(Transform placeForUi, ProfilePlayer profilePlayer)
+        public MainMenuController(Transform placeForUi, ProfilePlayer profilePlayer, UnityAdsService adsService, IAPService iapService)
         {
             _profilePlayer = profilePlayer;
             _view = LoadView(placeForUi);
-            _view.Init(StartGame, ShowSettings);
+            _view.Init(StartGame, ShowSettings, GetRewarded, BuyGoods);
+
+            _adsService = adsService;
+            _iapService = iapService;
         }
 
         private MainMenuView LoadView(Transform placeForUi)
@@ -33,5 +40,23 @@ namespace Ui
 
         private void ShowSettings() =>
             _profilePlayer.CurrentState.Value = GameState.Settings;
+
+        private void GetRewarded() =>
+            _adsService.RewardedPlayer.Play();
+        
+
+        private void BuyGoods()
+        {
+            if (_iapService.IsInitialized) OnIapInitialized();
+            else _iapService.Initialized.AddListener(OnIapInitialized);
+        }
+
+        protected override void OnDispose() 
+        {
+            _iapService.Initialized.RemoveListener(OnIapInitialized);
+        }
+
+        private void OnIapInitialized() => _iapService.Buy("1_test");
+
     }
 }
--- Assets/_Root/Scripts/Ui/MainMenuView.cs
+++ Assets/_Root/Scripts/Ui/MainMenuView.cs
@@ -8,18 +8,24 @@ namespace Ui
     {
         [SerializeField] private Button _buttonStart;
         [SerializeField] private Button _buttonSettings;
+        [SerializeField] private Button _buttonRewarded;
+        [SerializeField] private Button _buttonBuy;
 
 
-        public void Init(UnityAction startGame, UnityAction showSettings)
+        public void Init(UnityAction startGame, UnityAction showSettings, UnityAction getRewarded, UnityAction buyGoods)
         {
             _buttonStart.onClick.AddListener(startGame);
             _buttonSettings.onClick.AddListener(showSettings);
+            _buttonRewarded.onClick.AddListener(getRewarded);
+            _buttonBuy.onClick.AddListener(buyGoods);
         }
 
         public void OnDestroy()
         {
             _buttonStart.onClick.RemoveAllListeners();
             _buttonSettings.onClick.RemoveAllListeners();
+            _buttonRewarded.onClick.RemoveAllListeners();
+            _buttonBuy.onClick.RemoveAllListeners();
         }
     }
 }
