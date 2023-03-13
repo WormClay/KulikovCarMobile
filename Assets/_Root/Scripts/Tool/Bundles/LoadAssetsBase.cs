3e7c0bc30a923ce7df68b37fea2fa20f57859ccf
alexandr.kulikov <a.kulikov@tascredit.kz>
2023-03-14 00:38:26 +0600

    Lesson9
    Lesson9

---
+++ Assets/_Root/Scripts/Tool/Bundles/LoadAssetsBase.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+namespace Tool.Bundles.Examples
+{
+    internal class LoadAssetsBase : MonoBehaviour
+    {
+        private const string UrlAssetBundleSprites = "https://drive.google.com/uc?export=download&id=1rQzWdcChHhJJBTe4rf1D0Kwi1a43jxWR";
+
+        [SerializeField] private DataSpriteBundle _dataSpriteBundle;
+
+        private AssetBundle _spritesAssetBundle;
+
+        protected IEnumerator DownloadAndSetAssetBundles()
+        {
+            yield return GetSpritesAssetBundle();
+
+            if (_spritesAssetBundle != null)
+                SetSpriteAssets(_spritesAssetBundle);
+            else
+                Debug.LogError($"AssetBundle {nameof(_spritesAssetBundle)} failed to load");
+        }
+
+        private IEnumerator GetSpritesAssetBundle()
+        {
+            UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(UrlAssetBundleSprites);
+
+            yield return request.SendWebRequest();
+
+            while (!request.isDone)
+                yield return null;
+
+            StateRequest(request, out _spritesAssetBundle);
+        }
+
+        private void StateRequest(UnityWebRequest request, out AssetBundle assetBundle)
+        {
+            if (request.error == null)
+            {
+                assetBundle = DownloadHandlerAssetBundle.GetContent(request);
+                Debug.Log("Complete");
+            }
+            else
+            {
+                assetBundle = null;
+                Debug.LogError(request.error);
+            }
+        }
+
+        private void SetSpriteAssets(AssetBundle assetBundle)
+        {
+                _dataSpriteBundle.Image.sprite = assetBundle.LoadAsset<Sprite>(_dataSpriteBundle.NameAssetBundle);
+        }
+    }
+}
